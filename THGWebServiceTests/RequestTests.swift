f051a22f2197722fb2a94ba58c282ab5261a0451
Angelo Di Paolo <angelod101@gmail.com>
2015-03-11 16:07:41 -0400

    add unit tests to cover Request functionality

---
+++ THGWebServiceTests/RequestTests.swift
@@ -0,0 +1,62 @@
+//
+//  RequestTests.swift
+//  THGWebService
+//
+//  Created by Angelo Di Paolo on 3/11/15.
+//  Copyright (c) 2015 TheHolyGrail. All rights reserved.
+//
+
+import Foundation
+import THGWebService
+import XCTest
+
+/**
+ Tests the functionality of the Request struct.
+*/
+class RequestTests: XCTestCase {
+    
+    /**
+     Creates a Request value for testing.
+    */
+    static func CreateTestRequest() -> Request {
+        let url = "http://httpbin.org/get"
+        var request = Request(.GET, url: url)
+        request.headers["foo"] = "bar"
+        return request
+    }
+    
+    /**
+     Test Request's conformance to URLRequestConstructible.
+    */
+    func testConstructURLRequest() {
+        let request = RequestTests.CreateTestRequest()
+        let urlRequest = request.constructURLRequest()
+        
+        XCTAssertEqual(urlRequest.HTTPMethod!, request.method.rawValue)
+        
+        for (name, value) in request.headers {
+            let resultingValue = urlRequest.valueForHTTPHeaderField(name)!
+            XCTAssertEqual(value, resultingValue)
+        }
+    }
+    
+    /**
+     Test HTTP header computed properties and constants.
+    */
+    func testHeaderProperties() {
+        let contentType = "application/json"
+        let userAgent = "user agent value"
+        
+        var request = RequestTests.CreateTestRequest()
+        request.contentType = contentType
+        request.userAgent = userAgent
+        
+        XCTAssertEqual(request.headers["Content-Type"]!, contentType)
+        XCTAssertEqual(request.headers["User-Agent"]!, userAgent)
+        XCTAssertEqual(Request.Headers.userAgent, "User-Agent")
+        XCTAssertEqual(Request.Headers.contentType, "Content-Type")
+        XCTAssertEqual(Request.Headers.accept, "Accept")
+        XCTAssertEqual(Request.Headers.cacheControl, "Cache-Control")
+    }
+    
+}
