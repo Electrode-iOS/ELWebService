38b873a088a019b500e5ee7d2117c205dd820f86
Angelo Di Paolo <angelod101@gmail.com>
2016-03-10 16:21:19 -0500

    add source documentation for mocking API

--- Source/Core/Mocking.swift
+++ Source/Core/Mocking.swift
@@ -10,33 +10,51 @@ import Foundation
 
 // MARK: - MockableResponse
 
+/// Defined an interface for mocking data task result data for a given request.
 public protocol MockableDataTaskResult {
+    /**
+     Returns data that will be passed to the data task's completion handler
+     - parameter request: The request that the data task result is responding to.
+     - returns: Data that will be passed to the completion handler of the task.
+    */
     func dataTaskResult(request: URLRequestEncodable) -> (NSData?, NSURLResponse?, NSError?)
 }
 
 // MARK: - MockResponse
 
+/// Encapsulates the meta and body data of a response.
 public struct MockResponse {
     enum Error: ErrorType {
         case InvalidURL
     }
     
+    /// HTTP status code.
     public let statusCode: Int
+ 
+    /// Response body data
     public var data: NSData?
+    
+    /// Response URL
     public var url: NSURL?
+    
+    /// HTTP header fields
     public var headers: [String : String]?
+    
+    /// Version of the HTTP response as represented by the server. Typically this is represented as "HTTP/1.1".
     public var version: String?
     
+    /// Create a mock response with a given status code.
     public init(statusCode: Int) {
         self.statusCode = statusCode
     }
     
+    /// Create a mock response with a status code and response body data.
     public init(statusCode: Int, data: NSData) {
         self.init(statusCode: statusCode)
         self.data = data
     }
     
-    /// Defines a stubbed JSON object to use as the stubbed response body data.
+    /// Create a mocked response with a  JSON object to use as the stubbed response body data.
     public init(statusCode: Int, json: AnyObject) {
         self.init(statusCode: statusCode)
         self.data = try? NSJSONSerialization.dataWithJSONObject(json, options: NSJSONWritingOptions(rawValue: 0))
@@ -44,6 +62,7 @@ public struct MockResponse {
 }
 
 extension MockResponse: MockableDataTaskResult {
+    /// Creates a data task result from the mock response data
     public func dataTaskResult(request: URLRequestEncodable) -> (NSData?, NSURLResponse?, NSError?) {
         guard let responseURL = url ?? request.urlRequestValue.URL else {
             return (nil, nil, Error.InvalidURL as NSError)
@@ -54,9 +73,17 @@ extension MockResponse: MockableDataTaskResult {
     }
 }
 
+extension NSURLResponse: MockableDataTaskResult {
+    /// Creates a data task result with NSURLResponse instance as the response parameter
+    public func dataTaskResult(request: URLRequestEncodable) -> (NSData?, NSURLResponse?, NSError?) {
+        return (nil, self, nil)
+    }
+}
+
 // MARK: - Mocking Errors
 
 extension NSError: MockableDataTaskResult {
+    /// Creates a data task result with NSError instance as the error parameter
     public func dataTaskResult(request: URLRequestEncodable) -> (NSData?, NSURLResponse?, NSError?) {
         return (nil, nil, self)
     }
@@ -64,6 +91,7 @@ extension NSError: MockableDataTaskResult {
 
 // MARK: - MockSession
 
+/// Implements the `Session` protocol and provides an API for adding mocked responses as stubs.
 public class MockSession: Session {
     typealias Stub = (MockableDataTaskResult, (URLRequestEncodable) -> (Bool))
     lazy var stubs = [Stub]()
@@ -72,6 +100,7 @@ public class MockSession: Session {
         
     }
     
+    /// Creates a data task for a given request and calls the completion handler on a background queue.
     public func dataTask(request request: URLRequestEncodable, completion: (NSData?, NSURLResponse?, NSError?) -> Void) -> DataTask {
         requestSent(request)
         
@@ -90,14 +119,34 @@ public class MockSession: Session {
     
     // MARK: Stubbing API
     
+    /**
+     Add a stub that will be used as the response of a request.
+    
+     - parameter response: A mockable data task result that provides the mocked 
+     response value.
+     - parameter requestMatcher: A matcher closure that determines if the mocked 
+     response is used as a response stub for a given request.
+    */
     public func addStub(response: MockableDataTaskResult, requestMatcher: (URLRequestEncodable) -> (Bool)) {
         stubs.append((response, requestMatcher))
     }
-    
+
+    /**
+     Add a stub that will be used as the response of any request. 
+     The added stub will match any request.
+     
+     - parameter response: A mockable data task result that provides the mocked
+     response value.
+    */
     public func addStub(response: MockableDataTaskResult) {
         stubs.append((response, { _ in return true}))
     }
     
+    /**
+     Retrieve a response stub for a given request.
+     
+     - parameter request: The request that needs a stubbed response.
+     */
     public func stubbedResponse(request request: URLRequestEncodable) -> (NSData?, NSURLResponse?, NSError?) {
         for (response, matcher) in stubs.reverse() where matcher(request) {
             return response.dataTaskResult(request)
@@ -121,6 +170,7 @@ public class RequestRecordingSession: MockSession {
 
 // MARK: - Data Task Mocking
 
+/// A concrete implementation of DataTask for mocking purposes.
 public final class MockDataTask: DataTask {
     private(set) public var state = NSURLSessionTaskState.Suspended
     
